58b8e37ef8f3e76a913d1d0fcd31470489558251
Yuh <huypham3188@gmail.com>
2024-12-07 18:30:08 +0700

    UI schedule

--- backend/app/blueprints/flights/routes.py
+++ backend/app/blueprints/flights/routes.py
@@ -1,4 +1,16 @@
 from . import flights_bp, models
+from app.blueprints.auth import decorators
+from flask import render_template, jsonify, request
+from flask_login import current_user
 
-
+@flights_bp.route("/schedule", methods=['get', 'post'])
+@decorators.flight_manager_required
+def show_routes():
+    if request.method.__eq__('GET'):
+        return render_template("flights/show.html")
+    
+@flights_bp.route("/schedule/<id>", methods=['get'])
+@decorators.flight_manager_required
+def schedule(id):
+    return render_template("flights/schedule.html", id=id)
 
