af20f494a4c4d2f95a56193af664c13fb109433f
Yuh <huypham3188@gmail.com>
2024-12-08 18:39:45 +0700

    update schedule

--- backend/app/blueprints/flights/dao.py
+++ backend/app/blueprints/flights/dao.py
@@ -32,6 +32,34 @@ def add_route(
         print(f"Failed to add new route: {e}")
         return None
     
+def add_flight(
+    route_id,
+    depart_time,
+    arrive_time,
+    aircraft_id,
+):
+    # Tạo đối tượng Route mới
+    new_flight = Route(
+        route_id=route_id,
+        depart_time=depart_time,
+        arrive_time=arrive_time,
+        aircraft_id=aircraft_id,
+    )
+
+    # Thêm vào session của SQLAlchemy
+    db.session.add(new_flight)
+    
+    try:
+        #commit dữ liệu vào cơ sở dữ liệu
+        db.session.commit()
+        print("New flight added successfully!")
+        return new_flight
+    except Exception as e:
+        # Rollback nếu có lỗi xảy ra
+        db.session.rollback()
+        print(f"Failed to add new route: {e}")
+        return None
+    
 def load_routes(kw=None, page=None):
     query = Route.query
     if kw:
@@ -51,6 +79,20 @@ def count_routes(kw=None):
 
 def get_route_by_id(id):
     return Route.query.get(id)
-    
 
+# not stopover airport
+def load_stopover_airport():
+    return Airport.query.all()
+
+def load_flights(page=None):
+    query = Flight.query.order_by(Flight.id.desc())
+    
+    page_size = app.config['PAGE_SIZE']
+    start = (page - 1) * page_size
+    query = query.slice(start, start + page_size)
+
+    return query.all() 
+
+def count_flights():
+    return Flight.query.count()
 
--- backend/app/blueprints/flights/routes.py
+++ backend/app/blueprints/flights/routes.py
@@ -1,6 +1,6 @@
 from . import flights_bp, models
 from app.blueprints.auth import decorators
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify, request, redirect
 from flask_login import current_user
 from . import dao
 import math
@@ -17,11 +17,31 @@ def show_routes():
         total_elements = dao.count_routes(kw)
     
         return render_template("flights/show.html", routes=routes, pages=math.ceil(total_elements / app.config['PAGE_SIZE']),
-                               current_page=int(page), kw=kw if kw != None else '')
+                               current_page=int(page), kw=kw if kw else '')
+    
+    if request.method.__eq__('POST'):
+        data = request.form.copy()
+        print(data)
+       
+        return redirect(f'/schedule/{data['route_id']}')
 
 @flights_bp.route("/schedule/<id>", methods=['get'])
 @decorators.flight_manager_required
 def schedule(id):
     route = dao.get_route_by_id(id)
-    return render_template("flights/schedule.html", id=id, route=route)
+    page = request.args.get('page')
+    if page:
+        flights = dao.load_flights(page=int(page))
+        total_elements = dao.count_flights()
+    else:
+        flights = None
+        total_elements = None
+        
+        
+    # not stopover airport
+    airports = dao.load_stopover_airport()
+    
+    
+    return render_template("flights/schedule.html", id=id, route=route, airports=airports, flights=flights, current_page=int(page) if page else '',
+                           pages=math.ceil(total_elements / app.config['PAGE_SIZE']) if total_elements else '')
 
